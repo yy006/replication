e87a692c6be37ffdf019a4c22a2d42dce93d253d
yy006 <y1u2i3t4o@icloud.com>
2023-11-10 10:36:13 +0900

    plot IPonly

--- IP2Vec/model_IP2vec_imoken1122/tsne_dbscan_clustering_v2.py
+++ IP2Vec/model_IP2vec_imoken1122/tsne_dbscan_clustering_v2.py
@@ -6,35 +6,41 @@ import numpy as np
 from sklearn.manifold import TSNE
 from sklearn.cluster import DBSCAN
 
-# ファイルパスを適切に設定してください
-dataset = "CIDDS-001" # CTU-13_Scenario
-file_path = f'result/{dataset}/pth_model_w2v_v2w/model_w2v_v2w_10000.pth'
-
 # ファイルをロード
+dataset = "CIDDS-001" # CTU-13_Scenario
+file_path = f'result/{dataset}/pth_model_w2v_v2w/model_w2v_v2w_500000.pth'
+
 saved_data = torch.load(file_path, map_location=torch.device('cpu'))
+
+# v2w辞書を取得
+v2w = saved_data['v2w']
 
 # Embeddingsを取得
 embeddings = saved_data['model_state']['u_embedding.weight'].numpy()
 
+# 文字列型のワードと対応するベクトルのみを選択
+filtered_indices = [index for index, word in v2w.items() if isinstance(word, str)]
+filtered_embeddings = embeddings[filtered_indices]
+filtered_words = [v2w[i] for i in filtered_indices]
+
 # t-SNEで次元削減
 tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
-tsne_results = tsne.fit_transform(embeddings)
+tsne_results = tsne.fit_transform(filtered_embeddings)
 
 # DBSCANでクラスタリング
 dbscan = DBSCAN(eps=0.5, min_samples=5)
 dbscan_labels = dbscan.fit_predict(tsne_results)
 
-# v2w辞書を取得
-v2w = saved_data['v2w']
-
 # プロット用のデータフレームを作成
 df = pd.DataFrame(tsne_results, columns=['x', 'y'])
-df['label'] = [v2w[i] for i in range(len(tsne_results))]  # データポイントのワード
+df['word'] = filtered_words  # フィルタリングされたワードをデータフレームに追加
 df['cluster'] = dbscan_labels
 
 # インタラクティブなプロットを作成
-fig = px.scatter(df, x='x', y='y', color='cluster', text='label',
-                 hover_data=['label'], title='t-SNE and DBSCAN Clustering')
+#fig = px.scatter(df, x='x', y='y', color='cluster', text='word',
+#                 hover_data=['word'], title='Filtered IP Address t-SNE and DBSCAN Clustering')
 
+fig = px.scatter(df, x='x', y='y', color='cluster',
+                 hover_data=['word'], title='Filtered IP Address t-SNE and DBSCAN Clustering')
 # プロットを表示
 fig.show()
