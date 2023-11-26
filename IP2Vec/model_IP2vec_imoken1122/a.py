79b10bf8dbb921f032743afeb2f94421f3b14300
yy006 <y1u2i3t4o@icloud.com>
2023-11-27 04:51:01 +0900

    Correct local plots and improve plot accuracy by changing tsne parameters

--- IP2Vec/model_IP2vec_imoken1122/a.py
+++ IP2Vec/model_IP2vec_imoken1122/a.py
@@ -1,3 +1,4 @@
+'''
 import torch
 print(torch.__version__)
 print(torch.cuda.is_available())
@@ -5,3 +6,36 @@ import torch
 print(torch.version.cuda)
 print(torch.backends.cudnn.version())
 print(torch.backends.cudnn.enabled)
+'''
+'''
+import sklearn.cluster
+'''
+import numpy as np
+import pandas as pd
+import torch
+import preprocess
+import trainer
+import model
+import data_preprocess
+
+import re
+
+# 以下のコードは、選択した列で特定の条件を満たす行をフィルタリングし、それらの行を表示します。
+
+# 正規表現パターン: 192.168.x.y で、yは0から16まで
+pattern = r'^192\.168\.\d+\.(1[0-6]|[0-9])$'
+
+batch_size = 1024
+path = 'dataset/CIDDS-001/traffic/OpenStack/CIDDS-001-internal-week1.csv'
+preprocessor = data_preprocess.DataPreprocessor(path)
+features_to_include = ['Src IP Addr', 'Dst IP Addr', 'Proto', 'Src Pt', 'Dst Pt', 'class', 'attackType', 'attackID', 'attackDescription']
+processed_df = preprocessor.preprocess(num_rows=10000000, features=features_to_include) #start_date="2017-03-16 00:00:00"
+
+# 'Src IP Addr' 列で '192.168' を含む行のみをフィルタリング
+filtered_df = processed_df[processed_df['Src IP Addr'].str.contains(pattern, regex=True)]
+
+# 各ユニークな 'Src IP Addr' の出現回数をカウント
+ip_counts = filtered_df['Src IP Addr'].value_counts()
+
+# 結果を表示
+print(ip_counts)
--- IP2Vec/model_IP2vec_imoken1122/tsne_plot_local.py
+++ IP2Vec/model_IP2vec_imoken1122/tsne_plot_local.py
@@ -1,31 +1,129 @@
 import torch
 import numpy as np
 from sklearn.manifold import TSNE
-from sklearn.cluster import DBSCAN
 import matplotlib.pyplot as plt
+import data_preprocess
 
-# ファイルパスを適切に設定してください
+# Src IP Addr列の要素でフィルタリングするためにdfを取得
+path = 'dataset/CIDDS-001/traffic/OpenStack/CIDDS-001-internal-week1.csv'
+preprocessor = data_preprocess.DataPreprocessor(path)
+features_to_include = ['Src IP Addr', 'Dst IP Addr', 'Proto', 'Src Pt', 'Dst Pt', 'class', 'attackType', 'attackID', 'attackDescription']
+processed_df = preprocessor.preprocess(num_rows=10000000, features=features_to_include) #start_date="2017-03-16 00:00:00"
+
+
+# pthファイルパスの設定
 dataset = "CIDDS-001" # CTU-13_Scenario
 file_path = f'result/{dataset}/pth_model_w2v_v2w/opweek1/model_checkpoint_epoch_0.pth'
 
 # ファイルをロード
 saved_data = torch.load(file_path, map_location=torch.device('cpu'))
+'''
+#全データを次元削減した後に、192.168を含み、Src IP Addr列にも含まれるもののみをプロットする
+# v2w辞書とEmbeddingsを取得
+v2w = saved_data['v2w']
+embeddings = saved_data['model_state']['u_embedding.weight'].numpy()
+
+# Src IP Addr列に出現するユニークなIPアドレスを取得
+unique_ips = processed_df['Src IP Addr'].unique()
+
+# すべてのデータでt-SNEで次元削減
+perplexity_value = min(30, len(embeddings) - 1)
+tsne = TSNE(n_components=2, verbose=1, perplexity=perplexity_value, n_iter=300)
+tsne_results = tsne.fit_transform(embeddings)
+
+# 192.168を含み、Src IP Addr列にも含まれるインデックスを抽出
+indices_to_plot = [i for i, word in v2w.items() if(word in unique_ips)and(isinstance(word, str) and word.startswith('192.168'))]
+
+# t-SNE結果をプロット
+plt.figure(figsize=(12, 8))
+
+# 192.168を含み、Src IP Addr列にも含まれるデータポイントのみプロット
+for i in indices_to_plot:
+    plt.scatter(tsne_results[i, 0], tsne_results[i, 1], marker='.')
+    plt.annotate(v2w[i], (tsne_results[i, 0], tsne_results[i, 1]), fontsize=8)
+
+plt.title('Src IP Addresses - t-SNE Visualization')
+plt.xlabel('t-SNE axis 1')
+plt.ylabel('t-SNE axis 2')
+plt.show()
+'''
+
+'''
+# 192.168を含み、Src IP Addr列にも含まれるデータのみを次元削減
+# v2w辞書とEmbeddingsを取得
+v2w = saved_data['v2w']
+embeddings = saved_data['model_state']['u_embedding.weight'].numpy()
+
+# Src IP Addr列に出現するユニークなIPアドレスを取得
+unique_ips = processed_df['Src IP Addr'].unique()
+
+# v2w辞書から対応するインデックスを抽出
+# 192.168で始まるIPアドレスに対応するインデックスを抽出
+indices = [i for i, word in v2w.items() if(word in unique_ips)and(isinstance(word, str) and word.startswith('192.168'))]
+filtered_embeddings = embeddings[indices]
+
+# perplexityの値をフィルタリングされたサンプル数に合わせて調整
+perplexity_value = min(30, len(filtered_embeddings) - 1)
+
+# t-SNEで次元削減
+tsne = TSNE(n_components=2, verbose=1, perplexity=perplexity_value, n_iter=300)
+tsne_results = tsne.fit_transform(filtered_embeddings)
+
+# t-SNE結果をプロット
+plt.figure(figsize=(12, 8))
+plt.scatter(tsne_results[:, 0], tsne_results[:, 1], marker='.')
+
+# 各データポイントのIPアドレスをプロット
+for i, index in enumerate(indices):
+    plt.annotate(v2w[index], (tsne_results[i, 0], tsne_results[i, 1]), fontsize=8)
+
+plt.title('Src IP Addresses - t-SNE Visualization')
+plt.xlabel('t-SNE axis 1')
+plt.ylabel('t-SNE axis 2')
+plt.show()
+'''
 
 # v2w辞書とEmbeddingsを取得
 v2w = saved_data['v2w']
 embeddings = saved_data['model_state']['u_embedding.weight'].numpy()
 
-# 192.168で始まるIPアドレスに対応するインデックスを抽出
-indices = [i for i, word in v2w.items() if isinstance(word, str) and word.startswith('192.168')]
+# フィルタリングのためのIPアドレスのリストを定義
+ip_ranges = [
+    '192.168.100.3', '192.168.100.4', '192.168.100.5', '192.168.100.6',
+    '192.168.200.3', '192.168.200.4', '192.168.200.5', '192.168.200.8', '192.168.200.9',
+    '192.168.210.3', '192.168.210.4', '192.168.210.5'
+]
+ip_ranges += [f'192.168.220.{i}' for i in range(3, 17)]
+
+# v2w辞書から対応するインデックスを抽出
+indices = [i for i, word in v2w.items() if word in ip_ranges]
 filtered_embeddings = embeddings[indices]
 
 # perplexityの値をフィルタリングされたサンプル数に合わせて調整
 perplexity_value = min(30, len(filtered_embeddings) - 1)  # 30 または サンプル数-1 の小さい方を使用
 
-# その他のコード...
-
+'''
 # t-SNEで次元削減
 tsne = TSNE(n_components=2, verbose=1, perplexity=perplexity_value, n_iter=300)
+tsne_results = tsne.fit_transform(filtered_embeddings)
+
+# t-SNE結果をプロット
+plt.figure(figsize=(12, 8))
+plt.scatter(tsne_results[:, 0], tsne_results[:, 1], marker='.')
+
+# 各データポイントのIPアドレスをプロット
+for i, index in enumerate(indices):
+    plt.annotate(v2w[index], (tsne_results[i, 0], tsne_results[i, 1]), fontsize=8)
+
+plt.title('Src IP Addresses - t-SNE Visualization')
+plt.xlabel('t-SNE axis 1')
+plt.ylabel('t-SNE axis 2')
+plt.show()
+
+'''
+# 192.168を削除
+# t-SNEで次元削減
+tsne = TSNE(n_components=2, verbose=1, perplexity=10, n_iter=1500)
 tsne_results = tsne.fit_transform(filtered_embeddings)
 
 # t-SNE結果をプロット
